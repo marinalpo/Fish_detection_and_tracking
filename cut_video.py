3b125889146b6322421588d4d7cd18c56d5eb73f
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2019-04-17 20:12:25 +0200

    removed useless coments

--- cut_video.py
+++ cut_video.py
@@ -8,7 +8,7 @@ parser.add_argument('video_name', help = "Name of the video (without .MP4)")
 parser.add_argument('initial_second', type=int, help = "Second in the original video where the cut will begin")
 parser.add_argument('final_second', type=int, help = "Second in the original video where the cut will end")
 args = parser.parse_args()
-# Changed to lower caps, now it worklololos. we have checked
+# Changed to lower caps (.MP4 -> .mp4)
 myvideo = VideoFileClip(args.original_video_path+'/'+args.video_name+'.mp4')
 
 myvideoedited = myvideo.subclip(args.initial_second, args.final_second)

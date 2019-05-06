f0f220b9bf277b664d049d35709d04c5005afa88
benjamindorra <benjamin.dorra@yahoo.fr>
2019-05-06 16:43:19 +0000

    added a CLI and interpolation output

--- annotation/xmlTrack.py
+++ annotation/xmlTrack.py
@@ -1,41 +1,47 @@
 """
 
 Guide a Re3 tracker through a sequence of images from a cvat xml
-annotation file. Create a new annotation file including the
-result of the tracking. 
+annotation file. Create a new annotation/interpolation file
+including the result of the tracking.
+Optional arguments can be alternatively configured in the python program.
+
+Usage: vid2pics (-a | -t)[-i <images directory>][-x <xml file>]
+                [-o <output xml>][-r <re3 directory>][-g] 
+ 
+Options:
+ -i=<images directory> --images  Path to the input video
+ -x=<xml file> --xml  Path to an annotation xml
+ -o=<output xml> --output  Path to the output xml file
+ -a --annotation  Output in the cvat annotation format
+ -t --interpolation  Output in the cvat interpolation format
+ -r=<re3 directory> --re3dir  Path to the re3 directory
+ -g --graphic  Show the tracking process
 
 """
 
 import cv2
+from docopt import docopt
 import glob
 import numpy as np
 import os.path
 import sys
 import xml.etree.ElementTree as ET
 
-#Absolute path to the script directory
+#Paths to configure
 basedir = os.path.abspath(os.path.dirname(__file__))
 re3dir = "/home/utilisateur/builds/Re3/"
 imageDir = "/home/utilisateur/Desktop/CalaEgos7LTest/slice1/"
+imgFormat = ".jpg"
 xmlFile = "7_xmlTrackTest.xml"
+xmlPath = os.path.join(basedir, xmlFile)
 newXmlFile = "trackedAnnotation.xml"
+newXmlPath = os.path.join(basedir, newXmlFile)
 
-#Import and create a re3_tracker object
-sys.path.append(re3dir)
-from tracker import re3_tracker
-tracker = re3_tracker.Re3Tracker()
-
-#Load the images paths 
-imagePaths = sorted(glob.glob(os.path.join(imageDir, '*.jpg')))
-#Load the xml annotation file
-print("Parsing the xml file %s..."%os.path.join(
-    basedir, xmlFile), end='')
-xmlpath = os.path.join(basedir, xmlFile)
-xmlparse = ET.parse(xmlpath).getroot()
-print("[Done]")
-
-# in-place prettyprint formatter
 def indent(elem, level=0):
+    """
+    Prettyprint xml formatter
+    http://effbot.org/zone/element-lib.htm#prettyprint
+    """
     i = "\n" + level*"  "
     if len(elem):
         if not elem.text or not elem.text.strip():
@@ -78,70 +84,50 @@ def addBboxesToXml(elementTree, bboxes,
         box.set('ybr', str(bbox[3]))
         elementTree.getroot().findall("image")[pos].append(box)
 
-#Find the first annotated image
-#Can we not just take the first image ? TO CHECK
-annotatedImgs = xmlparse.findall('image')
-try:
-    firstAnnotated = int(annotatedImgs[0].get('name').split('.')[0])
-    imagePos = 0
-except:
-    raise Exception("No image found in the xml file !! "
-                    "(or their names are not numbers)")
-for i, image in enumerate(annotatedImgs):
-    imageNumber = int(image.get('name').split('.')[0])
-    if imageNumber < firstAnnotated:
-        firstAnnotated = imageNumber
-        imagePos = i
-#Find the position of the first annotated image in the list imagePaths
-#Can we not just use the id of the first annotated image ? TO CHECK        
-try:
-    firstAnnotationPos = firstAnnotated-\
-                         int(os.path.splitext(
-                             os.path.basename(imagePaths[0]))[0])
-except:
-    raise Exception("no image named <number>.jpg found in %s"%imageDir)
+def bboxInBounds(newXml): 
+    """Correcting boxes partially or completely out of bounds,
+    otherwise cvat might not be able to import the xml
+    """
+    def limitBbox(bbox):
+            xtags = ['xtl', 'xbr']
+            ytags = ['ytl', 'ybr']
+            for tag in xtags+ytags:
+                if float(bbox.get(tag))<0:
+                    bbox.set(tag, '0')
+            for tag in xtags:
+                if float(bbox.get(tag))>width:
+                    bbox.set(tag, str(width))
+            for tag in ytags:
+                if float(bbox.get(tag))>height:
+                    bbox.set(tag, str(height))
+                    
+    if arguments['--interpolation']:
+        original_size = newXml.getroot().find('meta').find('task').\
+                        find('original_size')
+        width = int(original_size.find('width').text)
+        height = int(original_size.find('height').text)
+        for track in newXml.findall('track'):
+            for bbox in track.findall('box'):
+                limitBbox(bbox)
+    if arguments['--annotation']:
+        for image in newXml.findall('image'):
+            width = int(image.get("width"))
+            height = int(image.get("height"))
+        for bbox in image.findall('box'):
+            limitBbox(bbox)
 
-###Window to visualize the tracking
-##cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+def drawBboxes(bboxes, image):
+    for bb,bbox in enumerate(bboxes):
+        color = cv2.cvtColor(np.uint8([[[bb * 255 / len(bboxes), 128, 200]]]),
+            cv2.COLOR_HSV2RGB).squeeze().tolist()
+        cv2.rectangle(image,
+                (int(bbox[0]), int(bbox[1])),
+                (int(bbox[2]), int(bbox[3])),
+                color, 2)
+    cv2.imshow('Image', image)
+    cv2.waitKey(1)
 
-#Create a new XML object
-newXml = ET.parse(xmlpath)
-
-print("Starting the tracking...")
-#Number of images to process
-numberImages = len(imagePaths)
-#The position in the annotated images list
-annotationPos = 0
-#The bounding box number, used for the ids
-idn=0
-#A list of bounding boxes ids
-ids = []
-#Start tracking on the first image where a fish has been annotated
-for ii,imagePath in enumerate(
-    imagePaths[firstAnnotationPos:], firstAnnotationPos):
-    #Show the progress
-    print("\rProcessing image %i/%i       "%(ii, numberImages), end='')
-    image = cv2.imread(imagePath)
-    # Tracker expects RGB, but opencv loads BGR.
-    imageRGB = image[:,:,::-1]
-
-    #Set the new bboxes to initiate trackers
-    newIdsBboxes = {}
-    if annotationPos<len(annotatedImgs):
-        if ii == int(annotatedImgs[annotationPos].get('id')):
-            annotatedImg = annotatedImgs[annotationPos]
-            for box in annotatedImg.findall('box'):
-                newId = box.get('label')+':'+box.get('occluded')+':'+str(idn)
-                idn+=1
-                if newId not in ids:
-                    x1 = float(box.get('xtl'))
-                    y1 = float(box.get('ytl'))
-                    x2 = float(box.get('xbr'))
-                    y2 = float(box.get('ybr'))
-                    newIdsBboxes[newId] = [x1, y1, x2, y2]
-                    ids.append(newId)
-            annotationPos+=1
-
+def getNewBboxes(tracker, ids, imageRGB, newIdsBboxes, newId):
     if len(ids)==1:
         #In case there is a single target to track
         #we use a different function
@@ -161,8 +147,12 @@ for ii,imagePath in enumerate(
     else:
         #All tracks are started, they don't need bounding boxes.
         bboxes = tracker.multi_track(ids, imageRGB)
-        
-    #Add the resulting bboxes to a xml
+
+    return bboxes
+
+def addBboxesAnnotation(newXml, bboxes, ids, annotatedImgs,
+                        annotationPos, ii, imagePaths):
+    """Add bboxes in a cvat annotation format xml tree"""
     if annotationPos<len(annotatedImgs) and\
         ii+1 == int(annotatedImgs[annotationPos].get('id')):
         #Case where the next image is already annotated
@@ -172,43 +162,152 @@ for ii,imagePath in enumerate(
         addBboxesToXml(newXml, bboxes, str(ii+1), ids,
         newImg=True, imageName=os.path.basename(imagePaths[ii]))
 
-##    #Add the bboxes to the image (only useful to visualize)
-##    for bb,bbox in enumerate(bboxes):
-##        color = cv2.cvtColor(np.uint8([[[bb * 255 / len(bboxes), 128, 200]]]),
-##            cv2.COLOR_HSV2RGB).squeeze().tolist()
-##        cv2.rectangle(image,
-##                (int(bbox[0]), int(bbox[1])),
-##                (int(bbox[2]), int(bbox[3])),
-##                color, 2)
-##    cv2.imshow('Image', image)
-##    cv2.waitKey(1)
+def addBboxesInterpolation(newXml, bboxes, ids, ii):
+    """Add bboxes in a cvat interpolation format xml tree"""
+    for bboxTags,bbox in zip(ids,bboxes):
+        tags = bboxTags.split(':')
+        idn = tags[2]
+        trackNumber = idn
+        #Compares the bbox id number to the number of tracks
+        if  int(idn) >= len(newXml.findall('track')):
+            #Create a new track element
+            track = ET.Element("track")
+            track.set("id", trackNumber)
+            track.set("label", tags[0])
+            newXml.getroot().append(track)
+        box = ET.Element("box")
+        box.set('frame', str(ii))
+        box.set('outside', '0')
+        box.set('occluded', tags[1])
+        box.set('keyframe', '1')
+        box.set('xtl', str(bbox[0]))
+        box.set('ytl', str(bbox[1]))
+        box.set('xbr', str(bbox[2]))
+        box.set('ybr', str(bbox[3]))
+        newXml.getroot().findall("track")[int(idn)].append(box)
 
-#Create a new line for the prompt
-print()
+def track(tracker, xmlParse, newXml, imagePaths):
+    """
+    Finds all bboxes in a cvat annotation file and tracks them
+    """
+    #Get all images with annotations
+    annotatedImgs = xmlParse.findall('image')
+    #Find the first annotated image
+    try:
+        firstAnnotated = int(annotatedImgs[0].get('id'))
+    except:
+        raise Exception("No annotated image in : %s"%xmlPath)
 
-#Correcting boxes partially or completely out of the frame,
-#otherwise cvat might not be able to import the xml
-for image in newXml.findall('image'):
-    for bbox in image.findall('box'):
-        width = int(annotatedImgs[0].get("width"))
-        height = int(annotatedImgs[0].get("height"))
-        xtags = ['xtl', 'xbr']
-        ytags = ['ytl', 'ybr']
-        for tag in xtags+ytags:
-            if float(bbox.get(tag))<0:
-                bbox.set(tag, '0')
-        for tag in xtags:
-            if float(bbox.get(tag))>width:
-                bbox.set(tag, str(width))
-        for tag in ytags:
-            if float(bbox.get(tag))>height:
-                bbox.set(tag, str(height))
+    print("Starting the tracking...")
+    #Number of images to process
+    numberImages = len(imagePaths)
+    #The position in the annotated images list
+    annotationPos = 0
+    #The bounding box number, used for the ids
+    idn=0
+    #A list of bounding boxes ids
+    ids = []
+    #Start tracking on the first image where a fish has been annotated
+    for ii,imagePath in enumerate(imagePaths[firstAnnotated:], firstAnnotated):
+        image = cv2.imread(imagePath)
+        # Tracker expects RGB, but opencv loads BGR.
+        imageRGB = image[:,:,::-1]
+
+        #Set the new bboxes to initiate trackers
+        newIdsBboxes = {}
+        if annotationPos<len(annotatedImgs):
+            if ii == int(annotatedImgs[annotationPos].get('id')):
+                annotatedImg = annotatedImgs[annotationPos]
+                for box in annotatedImg.findall('box'):
+                    newId = box.get('label')+':'+box.get('occluded')+\
+                            ':'+str(idn)
+                    idn+=1
+                    x1 = float(box.get('xtl'))
+                    y1 = float(box.get('ytl'))
+                    x2 = float(box.get('xbr'))
+                    y2 = float(box.get('ybr'))
+                    newIdsBboxes[newId] = [x1, y1, x2, y2]
+                    ids.append(newId)
+                annotationPos+=1
+
+        #Track the objects
+        bboxes = getNewBboxes(tracker, ids, imageRGB, newIdsBboxes, newId)
             
-#Prettify the xml
-indent(newXml.getroot())
-#Write the xml tree in a file
-print("Writing the xml tree in the file %s..."%os.path.join(
-    basedir, newXmlFile), end = '')
-newXml.write(os.path.join(basedir, newXmlFile))
-print("[Done]")
+        #Add the resulting bboxes to a xml
+        if arguments['--annotation']:
+            addBboxesAnnotation(newXml, bboxes, ids,
+                                annotatedImgs,annotationPos, ii, imagePaths)
+        if arguments['--interpolation']:
+            addBboxesInterpolation(newXml, bboxes, ids, ii)
+        if arguments['--graphic']:
+            #Add the bboxes to the image (only useful to visualize)
+            drawBboxes(bboxes, image)
+        #Show the progress
+        print("\rProcessing image %i/%i       "%(ii+1, numberImages), end='')
+    #Create a new line for the prompt
+    print()
+
+def main(re3dir, imageDir, imgFormat, xmlPath, newXmlPath):
+    #Import and create a re3_tracker object
+    if arguments['--re3dir']:
+        re3dir = arguments['--re3dir']
+    sys.path.append(re3dir)
+    from tracker import re3_tracker
+    tracker = re3_tracker.Re3Tracker()
+
+    #Load the images paths
+    if arguments['--images']:
+        imagePaths = arguments['--images']
+    else:
+        imagePaths = sorted(glob.glob(os.path.join(imageDir, '*'+imgFormat)))
+    #Load the xml annotation file
+    if arguments['--xml']:
+        xmlPath = arguments['xml']
+    print("Parsing the xml file %s..."%xmlPath, end='')
+    xmlParse = ET.parse(xmlPath).getroot()
+    print("[Done]")
+    #Create a new XML object
+    newXml = ET.parse(xmlPath)
+    
+    if arguments['--graphic']:
+        #Window to visualize the tracking
+        cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+    
+    #Remove images if we want an interpolation output
+    if arguments['--interpolation']:
+        meta = newXml.getroot().find('meta')
+        original_size = ET.Element('original_size')
+        width = ET.Element('width')
+        width.text = xmlParse.find('image').get('width')
+        height = ET.Element('height')
+        height.text = xmlParse.find('image').get('height')
+        original_size.append(width)
+        original_size.append(height)
+        task = meta.find('task')
+        task.append(original_size)
+        task.find('mode').text = 'interpolation'
+        for image in newXml.getroot().findall('image'):
+            newXml.getroot().remove(image)
+
+    #Track the bboxes and store the result in newXml
+    track(tracker, xmlParse, newXml, imagePaths)
+
+    #Limit the bboxes to the images bounds
+    bboxInBounds(newXml)
+                
+    #Prettify the xml
+    indent(newXml.getroot())
+    if arguments['--output']:
+        newXmlPath = arguments['--output']
+    #Write the xml tree in a file
+    print("Writing the xml tree in the file %s..."%newXmlPath, end = '')
+    newXml.write(newXmlPath)
+    print("[Done]")
+
+#checks that the script is called from the CLI
+if __name__ == '__main__':
+    #loads the CLI arguments in arguments
+    arguments = docopt(__doc__, version='xmlTrack 0.2')
+    main(re3dir, imageDir, imgFormat, xmlPath, newXmlPath)
+
     

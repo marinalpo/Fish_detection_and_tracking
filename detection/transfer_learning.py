df3da5dafb53e0174ae8a83a580baa0ab37e0560
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2019-05-25 16:39:10 +0200

    Added training

--- detection/transfer_learning.py
+++ detection/transfer_learning.py
@@ -30,6 +30,10 @@ import pickle
 from functools import partial
 
 """srun --mem 8G --gres=gpu:1,gmem:10G python transfer_learning.py  --learning_mode 0 --model /imatge/ppalau/work/Fishes/coco_resnet_50_map_0_335.pt --csv_train /imatge/ppalau/work/Fishes/test_image.csv  --classes /imatge/ppalau/work/Fishes/classes_mappings.csv --val /imatge/ppalau/work/Fishes/csv_val.csv"""
+"""
+/work/bdorra/annotations/ if you need data. The corresponding images are in /work/morros/fish/video_segments/
+""" 
+
 # TODO: try loading a model with resnet 152 
 
 # To try more than two
@@ -37,8 +41,69 @@ class LearningMode(IntEnum):
     FINETUNING = 1
     FEATURE_EXTRACTOR = 2
 
-def train_model():
-    print("")
+def train_model(retinanet, dataset_train, dataset_val, dataloader_train, dataloader_val):
+    optimizer = optim.Adam(retinanet.parameters(), lr=1e-5)
+
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
+
+    loss_hist = collections.deque(maxlen=500)
+
+    retinanet.module.freeze_bn()
+    epochs = 120
+    print('Num training images: {}'.format(len(dataset_train)))
+
+    for epoch_num in range(epochs):
+
+        retinanet.train()
+        retinanet.module.freeze_bn()
+        
+        epoch_loss = []
+
+        for iter_num, data in enumerate(dataloader_train):
+            try:
+                optimizer.zero_grad()
+
+                classification_loss, regression_loss = retinanet([data['img'].cuda().float(), data['annot']])
+
+                classification_loss = classification_loss.mean()
+                regression_loss = regression_loss.mean()
+
+                loss = classification_loss + regression_loss
+                
+                if bool(loss == 0):
+                    continue
+
+                loss.backward()
+
+                torch.nn.utils.clip_grad_norm_(retinanet.parameters(), 0.1)
+
+                optimizer.step()
+
+                loss_hist.append(float(loss))
+
+                epoch_loss.append(float(loss))
+
+                print('Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist)))
+                
+                del classification_loss
+                del regression_loss
+            except Exception as e:
+                print(e)
+                continue
+            print('Evaluating dataset')
+            mAP = csv_eval.evaluate(dataset_val, retinanet)
+
+        
+        scheduler.step(np.mean(epoch_loss))    
+
+        torch.save(retinanet.module, '{}_retinanet_{}.pt'.format(parser.dataset, epoch_num))
+
+    retinanet.eval()
+
+    torch.save(retinanet, 'model_final.pt'.format(epoch_num))
+
+
+
 def main(args=None):
     # ARGUMENT PARSE
     parser = argparse.ArgumentParser()
@@ -69,17 +134,25 @@ def main(args=None):
     pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
     # Load pretrained model
     retinanet = torch.load(args.model, pickle_module=pickle)
+    print("Retinanet model:")
 
+    print(retinanet.layer1)
+    
+    for param in retinanet.layer1.parameters():
+        print(param.requires_grad)
     retinanet.train(True)
+
 
     if(learning_mode == LearningMode.FINETUNING):
         # Begin with pretrained model and train it all
         print("Model load successfully, finetuning selcted")
         # Train the whole model
+        train_model(retinanet, dataset_train, dataset_val, dataloader_train,dataloader_val)
     elif(learning_mode == LearningMode.FEATURE_EXTRACTOR):
         print("Model load successfully, feature extraction selected")
         # Freeze ResNet and FPN layers and train the rest 
         
+        train_model(retinanet, dataset_train, dataset_val, dataloader_train,dataloader_val)
 
 if __name__ == '__main__':
     main()
--- utils/xml_to_csv.py
+++ utils/xml_to_csv.py
@@ -7,26 +7,41 @@ import xml.etree.ElementTree as ET
     Code borrowed from: 
     https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
 """
+base_image_path = "/work/morros/fish/video_segments"
+
 def xml_to_csv(path):
     xml_list = []
     print("EI")
     print(path)
     for xml_file in glob.glob(path + '/*.xml'):
+        
+        filename = xml_file.split('\\')
+        print(filename)
+        complete_videoname = filename[-1].split('_')
+        videoname = complete_videoname[0]
+        videopart = complete_videoname[1].split('.')[0]
+        print("Filename")
+        print(xml_file)
+        print("Video")
+        print(videoname)
+        print("Video part")
+        print(videopart)
+              
         tree = ET.parse(xml_file) # this should be the first line
         root = tree.getroot()
         print(root)
-        print("UEPA")
-        for track in root.findall('track'):
-            
+        for track in root.findall('image'):
+            print(track.attrib)
+            image_dict = track.attrib
+            image_name = image_dict['name']
+            complete_image_name = os.path.join(base_image_path, videoname,image_name)
+
             for box in track.findall('box'):
-                print("Params")
-                print(box.attrib)
                 box_dict = box.attrib
-                print("fish_type")
-                print(box[0].text)
-                fish_type = box[0].text
+                print("box_dict")
+                print(box_dict)
                 # The format that the CSVDataloader expects is frame, x1, y1, x2, y2, class_name
-                value = (int(box_dict['frame']), int(round(float(box_dict['xtl']))), int(round(float(box_dict['ytl']))), int(round(float(box_dict['xbr']))), int(round(float(box_dict['ybr']))), fish_type)
+                value = (str(complete_image_name), int(round(float(box_dict['xtl']))), int(round(float(box_dict['ytl']))), int(round(float(box_dict['xbr']))), int(round(float(box_dict['ybr']))), str(box_dict['label']))
                 xml_list.append(value)
     column_name = ['frame', 'xtl', 'ytl', 'xbr', 'ybr','fish_type']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
@@ -35,6 +50,7 @@ def xml_to_csv(path):
 
 def main():
     xml_path = os.path.join(os.getcwd(), '')
+    print(xml_path)
     xml_df = xml_to_csv(xml_path)
     xml_df.to_csv('raccoon_labels.csv', index=None)
     print('Successfully converted xml to csv.')
